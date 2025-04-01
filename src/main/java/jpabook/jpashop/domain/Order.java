a93f1248c6bfb7044447b5e662621822d7cfdaae
kkhyun <kkhyun9112@naver.com>
2025-04-01 15:00:31 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -47,5 +47,53 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
-    //연관관계 메서드
+    //연관관계 메서드 END
+
+    //생성 메서드 START
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        //
+        Order order = new Order();
+
+        //
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem oi : orderItems) {
+            order.addOrderItem(oi);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    //비즈니스 로직 START
+    /*
+    * 주문 취소
+    */
+    public void cancel() {
+        if( delivery.getStatus() == DeliveryStatus.COMP ) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem oi : orderItems) {
+            oi.cancel();
+        }
+    }
+
+    //조회 로직 START
+    /**
+     * 전체 주문 가격 조회
+     * @return
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for(OrderItem oi : orderItems) {
+            totalPrice += oi.getTotalPrice();
+        }
+        return totalPrice;
+
+//        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+    }
+
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,4 +24,24 @@ public class OrderItem {
     private int orderPirce; //주문가격
     private int count; //주문수량
 
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPirce, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPirce(orderPirce);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count); //취소시 재고 수량 원복
+    }
+
+    public int getTotalPrice() {
+        return getOrderPirce() * getCount();
+    }
 }
